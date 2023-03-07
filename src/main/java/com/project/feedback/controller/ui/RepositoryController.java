19752cb1eadf7708c49717dc1255bc1cc1832811
menuHwang <hmin0206@naver.com>
2023-03-07 15:35:39 +0900

    Repository 수정 컨트롤러 구현 및 ui 연결

--- src/main/java/com/project/feedback/controller/ui/RepositoryController.java
+++ src/main/java/com/project/feedback/controller/ui/RepositoryController.java
@@ -7,6 +7,7 @@ import com.project.feedback.service.RepositoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +22,13 @@ public class RepositoryController {
     public String save(RepositoryRequest request, Authentication auth) {
         UserEntity loginUser = findService.findUserByUserName(auth.getName());
         repositoryService.save(request, loginUser);
-        return "redirect:users/my";
+        return "redirect:/users/my";
+    }
+
+    @PostMapping("/{repositoryId}/edit")
+    public String edit(@PathVariable Long repositoryId, RepositoryRequest request, Authentication auth) {
+        UserEntity loginUser = findService.findUserByUserName(auth.getName());
+        repositoryService.update(repositoryId, request, loginUser);
+        return "redirect:/users/my";
     }
 }
