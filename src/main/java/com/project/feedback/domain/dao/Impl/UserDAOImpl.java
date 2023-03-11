f66ef17b01c41724696c9529be605f2b10427153
yoonnyeong <choiyn1994@naver.com>
2023-03-12 00:23:41 +0900

    UserService 구조 개선

---
+++ src/main/java/com/project/feedback/domain/dao/Impl/UserDAOImpl.java
@@ -0,0 +1,50 @@
+package com.project.feedback.domain.dao.Impl;
+
+import com.project.feedback.domain.dao.UserDAO;
+import com.project.feedback.domain.entity.UserEntity;
+import com.project.feedback.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserDAOImpl implements UserDAO {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDAOImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserEntity insertUser(UserEntity user){
+        UserEntity savedUser = userRepository.save(user);
+        return savedUser;
+    }
+
+    @Override
+    public Optional<UserEntity> findUserById(Long userId){
+        Optional<UserEntity> selectedUser = userRepository.findById(userId);
+        return selectedUser;
+    }
+
+
+    @Override
+    public Optional<UserEntity> findByUserName(String userName){
+        return userRepository.findByUserName(userName);
+    }
+
+    @Override
+    public Optional<UserEntity> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public Page<UserEntity> findAll(Pageable pageable){
+        return userRepository.findAll(pageable);
+    }
+}
