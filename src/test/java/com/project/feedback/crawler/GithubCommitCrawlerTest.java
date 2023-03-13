a04c52b3b59602c20d4a49bf6a2dd8d1b9aa7b6a
menuHwang <hmin0206@naver.com>
2023-03-13 18:05:27 +0900

    GithubCommitCrawler 수정으로 인한 테스트 수정

--- src/test/java/com/project/feedback/crawler/GithubCommitCrawlerTest.java
+++ src/test/java/com/project/feedback/crawler/GithubCommitCrawlerTest.java
@@ -1,10 +1,9 @@
 package com.project.feedback.crawler;
 
+import com.project.feedback.domain.entity.RepositoryEntity;
+import com.project.feedback.domain.entity.UserEntity;
+import com.project.feedback.fixture.RepositoryFixture;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,33 +12,35 @@ class GithubCommitCrawlerTest {
 
     @Test
     void execute() {
+        UserEntity dummy = UserEntity.builder().build();
         String algorithm = "https://github.com/Kyeongrok/java-algorithm";
-        String javaBook = "https://github.com/Kyeongrok/java-book";
-        List<String> addresses = new ArrayList<>(Arrays.asList(algorithm, javaBook));
-        List<Commit> commits = assertDoesNotThrow(() -> githubCommitCrawler.execute(addresses));
-        for (Commit commit : commits) {
-            assertTrue(algorithm.equals(commit.getAddress()) || javaBook.equals(commit.getAddress()));
-        }
+        RepositoryEntity repository = RepositoryFixture.repository(algorithm, dummy);
+        Commit commit = assertDoesNotThrow(() -> githubCommitCrawler.execute(repository));
+        assertEquals(algorithm, commit.getAddress());
     }
 
     @Test
     void execute_not_found_url() {
+        UserEntity dummy = UserEntity.builder().build();
         String wrongURL = "https://github.com/Kyeongrok/java-algorithm999";
-        List<String> addresses = new ArrayList<>(Arrays.asList(wrongURL));
+        RepositoryEntity repository = RepositoryFixture.repository(wrongURL, dummy);
 
-        List<Commit> commits = githubCommitCrawler.execute(addresses);
+        Commit commit = githubCommitCrawler.execute(repository);
 
-        assertEquals(wrongURL, commits.get(0).getAddress());
+        assertEquals(wrongURL, commit.getAddress());
+        assertNull(commit.getUrl());
+        assertNull(commit.getCommittedDatetime());
+        assertNull(commit.getTitle());
     }
 
     @Test
     void execute_git_url() {
-        String gitCloneURL= "https://github.com/Kyeongrok/java-algorithm.git";
+        UserEntity dummy = UserEntity.builder().build();
+        String gitCloneURL = "https://github.com/Kyeongrok/java-algorithm.git";
+        RepositoryEntity repository = RepositoryFixture.repository(gitCloneURL, dummy);
 
-        List<String> addresses = new ArrayList<>(Arrays.asList(gitCloneURL));
+        Commit commit = githubCommitCrawler.execute(repository);
 
-        List<Commit> commits = githubCommitCrawler.execute(addresses);
-
-        assertEquals(gitCloneURL, commits.get(0).getAddress());
+        assertEquals(gitCloneURL, commit.getAddress());
     }
 }
