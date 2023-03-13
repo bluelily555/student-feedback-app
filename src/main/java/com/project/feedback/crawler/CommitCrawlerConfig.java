e5883a3f4fc4f36949b87df4dce7945cd4590e04
menuHwang <hmin0206@naver.com>
2023-03-13 18:04:26 +0900

    CommitCrawler 설정 수정

--- src/main/java/com/project/feedback/crawler/CommitCrawlerConfig.java
+++ src/main/java/com/project/feedback/crawler/CommitCrawlerConfig.java
@@ -2,15 +2,13 @@ package com.project.feedback.crawler;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
 public class CommitCrawlerConfig {
-    @Bean
+    @Bean(name = "commitCrawlerExecutor")
     public Executor commitCrawlerExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(100);
