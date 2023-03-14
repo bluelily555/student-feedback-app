fd17f487614d597e591870a84815210160875bdc
menuHwang <hmin0206@naver.com>
2023-03-14 15:51:20 +0900

    Async 설정 및 전용 스레드풀 설정

---
+++ src/main/java/com/project/feedback/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.project.feedback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean(name = "notificationEventExecutor")
+    public Executor commitCrawlerExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("noti-exec-");
+        executor.initialize();
+        return executor;
+    }
+}
