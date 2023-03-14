e272b0d61bbdb95620b91dd95da17cbf62be044c
menuHwang <hmin0206@naver.com>
2023-03-14 15:51:47 +0900

    알림 생성 이벤트 및 이벤트 리스너 생성

---
+++ src/main/java/com/project/feedback/observer/notification/event/NotificationCreateEvent.java
@@ -0,0 +1,27 @@
+package com.project.feedback.observer.notification.event;
+
+import com.project.feedback.domain.entity.NotificationEntity;
+import com.project.feedback.domain.entity.UserEntity;
+import com.project.feedback.domain.enums.NotificationType;
+
+
+public class NotificationCreateEvent {
+    private final NotificationEntity notification;
+
+    private NotificationCreateEvent(NotificationEntity notification) {
+        this.notification = notification;
+    }
+
+    public NotificationEntity getNotification() {
+        return notification;
+    }
+
+    public static NotificationCreateEvent of(NotificationType type, Long sourceId, UserEntity targetUser, UserEntity fromUser) {
+        return new NotificationCreateEvent(NotificationEntity.builder()
+                .type(type)
+                .targetUser(targetUser)
+                .fromUser(fromUser)
+                .sourceId(sourceId)
+                .build());
+    }
+}
