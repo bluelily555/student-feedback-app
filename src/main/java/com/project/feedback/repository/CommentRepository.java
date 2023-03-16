d86c5c2a2384ba2310f80437a61cc60ae4c7907c
yoonnyeong <choiyn1994@naver.com>
2023-03-16 12:39:01 +0900

    #161 질문 목록 수정

--- src/main/java/com/project/feedback/repository/CommentRepository.java
+++ src/main/java/com/project/feedback/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.project.feedback.repository;
 
 
 import com.project.feedback.domain.entity.CommentEntity;
+import com.project.feedback.domain.enums.LikeContentType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     Page<CommentEntity> findByBoardEntityId(Long boardId, Pageable pageable);
    // List<CommentEntity> findByUserIdOrderByCreatedDateDesc(Long userId);
+   int countByBoardEntityId(Long boardId);
 
 }
