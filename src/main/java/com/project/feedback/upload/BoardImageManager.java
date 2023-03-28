49414b67d2f418bf38d28b7f1e0daeddf67bc377
menuHwang <hmin0206@naver.com>
2023-03-28 13:08:31 +0900

    BoardImageManager log 추가

--- src/main/java/com/project/feedback/upload/BoardImageManager.java
+++ src/main/java/com/project/feedback/upload/BoardImageManager.java
@@ -1,6 +1,7 @@
 package com.project.feedback.upload;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
 
@@ -11,10 +12,23 @@ public class BoardImageManager extends AbstractFileManager {
         super(imagesFolderPath);
     }
 
+    @Override
+    public String upload(MultipartFile file) {
+        log.info("질문 이미지 업로드");
+        return super.upload(file);
+    }
+
+    @Override
+    public boolean delete(String fileName) {
+        log.info("질문 이미지 삭제");
+        return super.delete(fileName);
+    }
+
     public boolean deleteAll(Collection<String> imageNames) {
+        log.info("질문 이미지 삭제");
         boolean result = true;
         for (String imageName : imageNames) {
-            result &= delete(imageName);
+            result &= super.delete(imageName);
         }
         return result;
     }
